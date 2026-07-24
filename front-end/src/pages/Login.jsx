f33e84fc72862a47135d7f5e08f1506dd2688287
Andres Castro <188876553+elcasti04@users.noreply.github.com>
2026-07-24 18:37:37 -0500

    ajustando el css del login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './style/Login.css'
+import './style/login.css'
 
 const Login = () => {
 	const [email, setEmail] = useState('');
